19319f616c8b8e8ddff05f28ebe25c36467b3d1a
nirmaan goyal <nirmaan.goyal.23@gmail.com>
2024-03-21 20:22:39 -0700

    first commit

---
+++ src/app/chart/page.tsx
@@ -0,0 +1,20 @@
+import Chart from "@/components/Charts/page";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Next.js Chart | CuracaoAdmin - Next.js Dashboard Template",
+  description:
+    "This is Next.js Chart page for CuracaoAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+};
+
+const BasicChartPage: React.FC = () => {
+  return (
+    <DefaultLayout>
+      <Chart />
+    </DefaultLayout>
+  );
+};
+
+export default BasicChartPage;
