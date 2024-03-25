5b68585f63a8b4b3b22e7e18457fdfb88da555b7
Rohan Sonawane <rohansonawane28@gmail.com>
2024-03-25 14:49:04 -0700

    Updated files

--- src/components/CampaignAdd/DatePicker/DatePickerTwo.tsx
+++ src/components/CampaignAdd/DatePicker/DatePickerTwo.tsx
@@ -19,7 +19,7 @@ const DatePickerTwo = () => {
   return (
     <div>
       <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-        Select date
+        End Date
       </label>
       <div className="relative">
         <input
